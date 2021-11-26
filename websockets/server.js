6ca4bc699efb2937f98606c4e218b1218a91b2d1
Karl MARQUES <kmarques@vetixy.com>
2021-11-26 10:18:53 +0100

    WIP websocket + web server

--- websockets/server.js
+++ websockets/server.js
@@ -1,13 +1,33 @@
-const { SocketAddress } = require("net");
+const { createServer } = require("http");
 const { WebSocketServer } = require("ws");
+const fs = require("fs/promises");
+const { constants } = require("fs");
+
+const server = createServer(function (req, res) {
+  fs.access("." + req.url, constants.R_OK)
+    .then(() => fs.readFile("." + req.url))
+    .then((data) => {
+      if (req.url.endsWith(".html")) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+      }
+      if (req.url.endsWith(".js")) {
+        res.writeHead(200, {
+          "Content-Type": "application/javascript; charset=utf-8",
+        });
+      }
+      res.write(data);
+      res.end();
+    })
+    .catch(() => res.writeHead(404));
+});
 
 const sockets = {};
 const messages = {};
-const server = new WebSocketServer({ port: 8080 }, () =>
+const ws = new WebSocketServer({ server }, () =>
   console.log("Listening on port 8080")
 );
 
-server.on("connection", (socket) => {
+ws.on("connection", (socket) => {
   socket.id = Date.now();
   sockets[socket.id] = socket;
   const event = {
@@ -33,3 +53,5 @@ server.on("connection", (socket) => {
     }
   });
 });
+
+server.listen(8080, () => console.log("Server is listening"));
